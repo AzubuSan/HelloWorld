feb19e3abb3035d5a031f627a1854012e1698954
AzubuSan <azubu.san@gmail.com>
2013-11-30 16:54:59 +0100

    Added Files

---
+++ HelloWorld/src/net/azubusan/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package net.azubusan.helloworld;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+
+	public static void main(String args[]) {
+
+		JOptionPane p = new JOptionPane();
+
+		p.showMessageDialog(null, "Hello World!");
+	}
+
+}
